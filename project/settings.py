9b568658a0613f69d707bb60512c0bf5940ccb40
JB-Tellez <jb.tellez@gmail.com>
2022-06-09 00:51:18 -0700

    adds CSRF_TRUSTED_ORIGINS

--- project/settings.py
+++ project/settings.py
@@ -175,3 +175,6 @@ SIMPLE_JWT = {
 
 CORS_ORIGIN_WHITELIST = tuple(env.list("ALLOWED_ORIGINS"))
 CORS_ALLOW_ALL_ORIGINS = env.bool("ALLOW_ALL_ORIGINS")
+CSRF_TRUSTED_ORIGINS = [
+    'https://cookie-stand-api-reference.herokuapp.com'
+]
